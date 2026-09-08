c06e370d5265927771ed7aab54d9f8e763119bcc
Travis Yunis <tyunis@jcphkvg9nd.hq.corp.viasat.com>
2026-09-08 13:03:02 -0700

    fix: configure GitHub Pages build and workflow

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: process.env.BUILD_BASE ?? '/',
   server: {
     port: 3000,
     open: true
